76e80925a4ebc269f579bcdf2cb72eeb2b33949e
ThibaudLafont <thibaudlafont@gmail.com>
2018-05-11 16:45:06 +0200

    Add identity for git

--- 00Scripts/gitignore.sh
+++ 00Scripts/gitignore.sh
@@ -1,3 +1,5 @@
 mkdir '/home/thib/.config/git'
 cp '/home/thib/Dev/Config/gitignore' '/home/thib/.config/git/ignore'
 
+git config --global user.email "thibaudlafont@gmail.com"
+git config --global user.name "ThibaudLafont"
